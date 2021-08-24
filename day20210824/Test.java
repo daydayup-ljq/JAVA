2c74a5153c80e5854f9ec02b27004e5a7251f144
daydayup-ljq <2177493804@qq.com>
2021-08-24 20:46:39 +0800

    练习5

---
+++ day20210824/Test.java
@@ -0,0 +1,31 @@
+package day20210824;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		String str="123";
+//		int n1=Integer.parseInt(str);
+//		System.out.println(n1);
+//		int a1=13;
+//		int a2=17;
+//		int a3=a1+a2;
+//		System.out.println("a3="+a3);
+//		int a4=38;
+//		int a5=a4-a3;
+//		System.out.println(a5);
+		
+//		 int i=1;
+//		 i=i++;
+//		 //1.int temp=i;
+//		 //2. i = i + 1
+//		 //3. i=temp;
+//		 System.out.println(i);
+		
+		int day =59;
+		int weeks=day/7;
+		int let=day%7;
+}
+
+}
